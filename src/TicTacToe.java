d9009ca230fd272c45156354052ce6dde06f5a5f
Ashlynn Martell <ashlynn.martell@gmail.com>
2025-02-08 14:30:11 -0600

    first commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToe {
+	int boardWidth = 600;
+	int boardHeight = 650; //50px for text panel at top
+
+	JFrame frame = new JFrame();
+	JLabel textLabel = new JLabel();
+	JPanel textPanel = new JPanel();
+
+
+	TicTacToe() {
+		frame.setVisible(true);
+		frame.setSize(boardWidth, boardHeight);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //click x it terminates
+		frame.setLayout(new BorderLayout());
+
+		textLabel.setBackground(Color.darkGray); 
+		textLabel.setForeground(Color.white);
+		textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+		textLabel.setHorizontalAlignment(JLabel.CENTER);
+		textLabel.setText("Tic-Tac-Toe");
+		textLabel.setOpaque(true);
+
+		textPanel.setLayout(new BorderLayout());
+		textPanel.add(textLabel);
+		frame.add(textPanel, BorderLayout.NORTH);
+
+	}
+}
