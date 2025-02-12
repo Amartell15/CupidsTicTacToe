a4e05467fd2580374c1ed85247c95c84b6d7fc76
Ashlynn Martell <ashlynn.martell@gmail.com>
2025-02-12 16:29:08 -0600

    Introduced player swap logic.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,28 +4,27 @@ import javax.swing.*;
 
 public class TicTacToe {
 	int boardWidth = 600;
-	int boardHeight = 650; //50px for text panel at top
+	int boardHeight = 650; // 50px for text panel at top
 
 	JFrame frame = new JFrame("Cupid's Tic-Tac-Toe");
 	JLabel textLabel = new JLabel();
 	JPanel textPanel = new JPanel();
 	JPanel boardPanel = new JPanel();
 
-	JButton [][] board = new JButton[3][3];
+	JButton[][] board = new JButton[3][3];
 	String playerX = "X";
 	String playerO = "O";
 	String currentPlayer = playerX;
-
 
 	TicTacToe() {
 		frame.setVisible(true);
 		frame.setSize(boardWidth, boardHeight);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //click x it terminates
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // click x it terminates
 		frame.setLayout(new BorderLayout());
 
-		textLabel.setBackground(Color.darkGray); 
+		textLabel.setBackground(Color.pink);
 		textLabel.setForeground(Color.white);
 		textLabel.setFont(new Font("Arial", Font.BOLD, 50));
 		textLabel.setHorizontalAlignment(JLabel.CENTER);
@@ -36,23 +35,32 @@ public class TicTacToe {
 		textPanel.add(textLabel);
 		frame.add(textPanel, BorderLayout.NORTH);
 
-		boardPanel.setLayout(new GridLayout(3,3));
+		boardPanel.setLayout(new GridLayout(3, 3));
 		boardPanel.setBackground(Color.darkGray);
 		frame.add(boardPanel);
 
-		for(int r = 0; r < 3; r++){
-			for(int c = 0; c < 3; c++){
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 3; c++) {
 				JButton tile = new JButton();
 				board[r][c] = tile;
 				boardPanel.add(tile);
 
-				tile.setBackground(Color.darkGray);
+				tile.setBackground(Color.pink);
 				tile.setForeground(Color.white);
 				tile.setFont(new Font("Arial", Font.BOLD, 120));
 				tile.setFocusable(false);
-				
+				// tile.setText(currentPlayer);
 
+				tile.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						JButton tile = (JButton) e.getSource();
+						tile.setText(currentPlayer);
 
+						currentPlayer = currentPlayer == playerX ? playerO : playerX; //ternary operator is short-hand for if-else
+						textLabel.setText(currentPlayer + "'s turn.");
+
+					}
+				});
 			}
 		}
 	}
